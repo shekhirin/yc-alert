96ac1254383ee6b643ee4856e51836fdd41c7b83
Alexey Shekhirin <a.shekhirin@gmail.com>
2021-08-26 20:34:20 +0300

    enable jobs_change function

--- change.py
+++ change.py
@@ -23,55 +23,55 @@ def jobs_change(diffs: List[List], previous_company: dict, current_company: dict
         'remove': []
     }
 
-    # for diff in diffs:
-    #     if diff[0] == 'add':
-    #         if diff[1] == ['data', 'jobs']:
-    #             for (_, job) in diff[2]:
-    #                 jobs['add'].append(job)
-    #     # elif diff[0] == 'change':
-    #     #     if diff[1][:2] == ['data', 'jobs'] and diff[1][-1] == 'apply':
-    #     #         idx = diff[1][2]
-    #     #         jobs['add'].append(current_company['data']['jobs'][idx])
-    #     #         jobs['remove'].append(previous_company['data']['jobs'][idx])
-    #     elif diff[0] == 'remove':
-    #         if diff[1] == ['data', 'jobs']:
-    #             for (_, job) in diff[2]:
-    #                 jobs['remove'].append(job)
+    for diff in diffs:
+        if diff[0] == 'add':
+            if diff[1] == ['data', 'jobs']:
+                for (_, job) in diff[2]:
+                    jobs['add'].append(job)
+        elif diff[0] == 'change':
+            if diff[1][:2] == ['data', 'jobs'] and diff[1][-1] == 'apply':
+                idx = diff[1][2]
+                jobs['add'].append(current_company['data']['jobs'][idx])
+                jobs['remove'].append(previous_company['data']['jobs'][idx])
+        elif diff[0] == 'remove':
+            if diff[1] == ['data', 'jobs']:
+                for (_, job) in diff[2]:
+                    jobs['remove'].append(job)
 
-    # jobs['add'] = unique_dicts(jobs['add'], 'apply')
-    # jobs['remove'] = unique_dicts(jobs['remove'], 'apply')
-    #
-    # print('JOBS:', jobs)
-    #
+    jobs['add'] = unique_dicts(jobs['add'], 'apply')
+    jobs['remove'] = unique_dicts(jobs['remove'], 'apply')
+
+    print('JOBS:', jobs)
+
     messages = []
-    #
-    # if len(jobs['add']) == 1:
-    #     job = jobs['add'][0]
-    #
-    #     messages.append(
-    #         f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
-    #         f'has just added new job opening for <a href="{job["apply"]}">{job["title"]}</a> in {job["location"]}'
-    #     )
-    # elif len(jobs['add']) > 1:
-    #     messages.append(
-    #         f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
-    #         f'has just added new job openings:<br<br><br>' +
-    #         '<br>'.join([f'- <a href="{x["apply"]}">{x["title"]}</a> in {x["location"]}' for x in jobs['add']])
-    #     )
-    #
-    # if len(jobs['remove']) == 1:
-    #     job = jobs['remove'][0]
-    #
-    #     messages.append(
-    #         f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
-    #         f'has just deleted job opening for {job["title"]} in {job["location"]}'
-    #     )
-    # elif len(jobs['remove']) > 1:
-    #     messages.append(
-    #         f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
-    #         f'has just deleted job openings:<br<br><br>' +
-    #         '<br>'.join([f'- <b>{x["title"]}</b> in {x["location"]}' for x in jobs['remove']])
-    #     )
+
+    if len(jobs['add']) == 1:
+        job = jobs['add'][0]
+
+        messages.append(
+            f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
+            f'has just added new job opening for <a href="{job["apply"]}">{job["title"]}</a> in {job["location"]}'
+        )
+    elif len(jobs['add']) > 1:
+        messages.append(
+            f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
+            f'has just added new job openings:<br<br><br>' +
+            '<br>'.join([f'- <a href="{x["apply"]}">{x["title"]}</a> in {x["location"]}' for x in jobs['add']])
+        )
+
+    if len(jobs['remove']) == 1:
+        job = jobs['remove'][0]
+
+        messages.append(
+            f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
+            f'has just deleted job opening for {job["title"]} in {job["location"]}'
+        )
+    elif len(jobs['remove']) > 1:
+        messages.append(
+            f'<a href="{current_company["data"]["links"][0]}">{current_company["data"]["name"]}</a> '
+            f'has just deleted job openings:<br<br><br>' +
+            '<br>'.join([f'- <b>{x["title"]}</b> in {x["location"]}' for x in jobs['remove']])
+        )
 
     return messages
 
